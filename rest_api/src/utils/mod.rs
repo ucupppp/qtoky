710998a933a92f4b1d180f27a54766dc0626af9a
ucuppppp <radithamzah12@gmail.com>
2025-06-17 11:16:57 +0700

    refactor(backend/utils): remove unused function.rs and clean up mod.rs

--- rest_api/src/utils/mod.rs
+++ rest_api/src/utils/mod.rs
@@ -1,1 +1,16 @@
-pub mod function;
+use bson::oid::ObjectId;
+use serde::Serializer;
+
+pub fn object_id_as_string<S>(id: &Option<ObjectId>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    match id {
+        Some(oid) => serializer.serialize_str(&oid.to_hex()),
+        None => serializer.serialize_none(),
+    }
+}
+
+pub fn string_id_to_obj_id(id: &str) -> Option<ObjectId> {
+    ObjectId::parse_str(id).ok()
+}
