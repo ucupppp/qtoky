643087cff74829252949dc0e4a7fc6f2219ec745
ucuppppp <radithamzah12@gmail.com>
2025-06-17 13:45:36 +0700

    feat(backend/utils): add hash and verify password for user

--- rest_api/src/utils/mod.rs
+++ rest_api/src/utils/mod.rs
@@ -1,3 +1,7 @@
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
+};
 use bson::oid::ObjectId;
 use serde::Serializer;
 
@@ -13,4 +17,25 @@ where
 
 pub fn string_id_to_obj_id(id: &str) -> Option<ObjectId> {
     ObjectId::parse_str(id).ok()
+}
+
+pub fn hash_password(password: &str) -> Result<String, argon2::password_hash::Error> {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+
+    let hash = argon2
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string();
+    Ok(hash)
+}
+
+pub fn verify_password(password: &str, password_hash: &str) -> bool {
+    let parsed_hash = PasswordHash::new(password_hash);
+    if let Ok(parsed_hash) = parsed_hash {
+        Argon2::default()
+            .verify_password(password.as_bytes(), &parsed_hash)
+            .is_ok()
+    } else {
+        false
+    }
 }
