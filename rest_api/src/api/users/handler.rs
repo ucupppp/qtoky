6b8dd0740e812279ab32fe07687b1c44350fa5a2
ucuppppp <radithamzah12@gmail.com>
2025-06-17 11:16:01 +0700

    feat(backend/rest/user): update get_user_handler and get_users_handler to use ServiceError and DTO

--- rest_api/src/api/users/handler.rs
+++ rest_api/src/api/users/handler.rs
@@ -1,36 +1,34 @@
-// src/api/users/handler.rs
-
-use crate::errors::error::ApiError;
-use crate::services::user_service::get_users_service;
-use actix_web::{HttpResponse, Result, web};
+use crate::errors::ApiError;
+use crate::models::user::UserResponse;
+use crate::services::user_service::{get_user_service, get_users_service};
+use actix_web::{
+    HttpResponse, Result,
+    web::{Data, Path},
+};
 use mongodb::Database;
 
-pub async fn get_user_handler(path: web::Path<i32>) -> Result<HttpResponse, ApiError> {
+pub async fn get_user_handler(
+    path: Path<String>,
+    db: Data<Database>,
+) -> Result<HttpResponse, ApiError> {
     let user_id = path.into_inner();
+    let user = get_user_service(&user_id, &db).await?;
 
-    if user_id == 0 {
-        return Err(ApiError::BadRequest("User ID tidak valid".to_string()));
-    }
-
-    if user_id == 404 {
-        return Err(ApiError::NotFound);
-    }
+    let user_response: UserResponse = user.into(); // konversi eksplisit dulu
 
     Ok(HttpResponse::Ok().json(serde_json::json!({
         "status": "success",
-        "data": {
-            "id": user_id,
-            "name": "John Doe"
-        }
+        "data": user_response
     })))
 }
 
-pub async fn get_users_handler(db: web::Data<Database>) -> Result<HttpResponse, ApiError> {
-    match get_users_service(&db).await {
-        Ok(users) => Ok(HttpResponse::Ok().json(serde_json::json!({
-            "status": "success",
-            "data": users
-        }))),
-        Err(_) => Err(ApiError::BadRequest("Gagal mengambil data user".into())),
-    }
+pub async fn get_users_handler(db: Data<Database>) -> Result<HttpResponse, ApiError> {
+    let users = get_users_service(&db).await?;
+
+    let users_response: Vec<UserResponse> = users.into_iter().map(UserResponse::from).collect();
+
+    Ok(HttpResponse::Ok().json(serde_json::json!({
+        "status": "success",
+        "data": users_response
+    })))
 }
