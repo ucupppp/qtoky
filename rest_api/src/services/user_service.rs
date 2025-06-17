8b99c63f6af1a4b481371f7794ef5f2410891fbf
ucuppppp <radithamzah12@gmail.com>
2025-06-17 11:16:57 +0700

    feat(backend/service): implement get_user_service and get_users_service with proper error handling

--- rest_api/src/services/user_service.rs
+++ rest_api/src/services/user_service.rs
@@ -1,17 +1,47 @@
+use crate::errors::ServiceError;
 use crate::models::user::User;
+use crate::utils::string_id_to_obj_id;
 use futures::stream::TryStreamExt;
-use mongodb::{Collection, Database, bson::doc, error::Error};
+use mongodb::{Collection, Database, bson::doc};
 
-pub async fn get_users_service(db: &Database) -> Result<Vec<User>, Error> {
+pub async fn get_users_service(db: &Database) -> Result<Vec<User>, ServiceError> {
     let collection: Collection<User> = db.collection("users");
 
-    let mut cursor = collection.find(doc! {}).await?;
+    let mut cursor = collection
+        .find(doc! {})
+        .await
+        .map_err(|e| ServiceError::DatabaseError(e.to_string()))?;
+
     let mut users: Vec<User> = Vec::new();
 
-    while let Some(user) = cursor.try_next().await? {
+    while let Some(user) = cursor
+        .try_next()
+        .await
+        .map_err(|e| ServiceError::DatabaseError(e.to_string()))?
+    {
         users.push(user);
     }
 
     Ok(users)
+}
+
+pub async fn get_user_service(user_id: &str, db: &Database) -> Result<User, ServiceError> {
+    let collection: Collection<User> = db.collection("users");
+
+    let object_id = match string_id_to_obj_id(user_id) {
+        Some(id) => id,
+        None => return Err(ServiceError::InvalidId("Invalid ID".into())),
+    };
+
+    let filter = doc! { "_id": object_id };
+
+    let user = collection
+        .find_one(filter)
+        .await
+        .map_err(|e| ServiceError::DatabaseError(e.to_string()))?;
+
+    user.ok_or_else(|| {
+        ServiceError::NotFound(format!("User dengan ID '{}' tidak ditemukan", user_id))
+    })
 }
 
