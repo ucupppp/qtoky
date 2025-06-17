949c6df76df4838bbb938fef6dd53a5308adf8ca
ucuppppp <radithamzah12@gmail.com>
2025-06-17 16:33:22 +0700

    feat(backend/service): adding new service to create new user

--- rest_api/src/services/user_service.rs
+++ rest_api/src/services/user_service.rs
@@ -1,6 +1,6 @@
 use crate::errors::ServiceError;
-use crate::models::user::User;
-use crate::utils::string_id_to_obj_id;
+use crate::models::user::{CreateUserDTO, User};
+use crate::utils::{hash_password, string_id_to_obj_id};
 use futures::stream::TryStreamExt;
 use mongodb::{Collection, Database, bson::doc};
 
@@ -45,3 +45,43 @@ pub async fn get_user_service(user_id: &str, db: &Database) -> Result<User, Serv
     })
 }
 
+pub async fn create_user_service(
+    payload: CreateUserDTO,
+    db: &Database,
+) -> Result<User, ServiceError> {
+    let collection: Collection<User> = db.collection("users");
+
+    let username = payload.username;
+    let email = payload.email;
+    let phone_number = payload.phone_number;
+    let mut password = payload.password.unwrap_or_else(|| "".to_string());
+
+    if password.trim().is_empty() {
+        password = username.clone();
+    }
+
+    let hashed_password = hash_password(&password)
+        .map_err(|e| ServiceError::HashingError(format!("Gagal hashing password: {}", e)))?;
+
+    let new_user = User {
+        id: None,
+        username,
+        email,
+        password_hash: hashed_password,
+        phone_number,
+    };
+
+    let result = collection.insert_one(&new_user).await;
+
+    match result {
+        Ok(_) => Ok(new_user),
+        Err(err) => {
+            if err.to_string().contains("E11000") {
+                return Err(ServiceError::Conflict(
+                    "Username atau Email sudah digunakan".to_string(),
+                ));
+            }
+            Err(ServiceError::DatabaseError(err.to_string()))
+        }
+    }
+}
