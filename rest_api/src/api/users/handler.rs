cdefb188093658aa4c74b5c22e7407e6b28973dd
ucuppppp <radithamzah12@gmail.com>
2025-06-18 08:23:00 +0700

    feat(backend/rest/user): adding new handler to editing user

--- rest_api/src/api/users/handler.rs
+++ rest_api/src/api/users/handler.rs
@@ -1,6 +1,8 @@
 use crate::errors::ApiError;
-use crate::models::user::{CreateUserDTO, UserResponse};
-use crate::services::user_service::{create_user_service, get_user_service, get_users_service};
+use crate::models::user::{CreateUserDTO, UpdateUserDTO, UserResponse};
+use crate::services::user_service::{
+    create_user_service, get_user_service, get_users_service, update_user_service,
+};
 use actix_web::{
     HttpResponse, Result,
     web::{Data, Json, Path},
@@ -29,7 +31,8 @@ pub async fn get_users_handler(db: Data<Database>) -> Result<HttpResponse, ApiEr
 
     Ok(HttpResponse::Ok().json(serde_json::json!({
         "status": "success",
-        "data": users_response
+        "data": users_response,
+        "code": 200
     })))
 }
 
@@ -39,8 +42,62 @@ pub async fn post_user_handler(
 ) -> Result<HttpResponse, ApiError> {
     let new_user = create_user_service(payload.into_inner(), &db).await?;
     let user_response: UserResponse = new_user.into();
+    Ok(HttpResponse::Created()
+        .append_header(("Location", format!("/users/{}", user_response.id)))
+        .json(serde_json::json!({
+            "status": "success",
+            "data": user_response,
+            "code": 201
+        })))
+}
+
+pub async fn patch_user_handler(
+    path: Path<String>,
+    payload: Option<Json<UpdateUserDTO>>,
+    db: Data<Database>,
+) -> Result<HttpResponse, ApiError> {
+    let user_id = path.into_inner();
+
+    let Some(json_payload) = payload else {
+        return Err(ApiError::BadRequest("Body tidak boleh kosong".into()));
+    };
+
+    let data = json_payload.into_inner();
+
+    // Validasi semua field kosong atau berisi string kosong
+    let no_fields = data
+        .username
+        .as_ref()
+        .map(|s| s.trim().is_empty())
+        .unwrap_or(true)
+        && data
+            .email
+            .as_ref()
+            .map(|s| s.trim().is_empty())
+            .unwrap_or(true)
+        && data
+            .phone_number
+            .as_ref()
+            .map(|s| s.trim().is_empty())
+            .unwrap_or(true)
+        && data
+            .password
+            .as_ref()
+            .map(|s| s.trim().is_empty())
+            .unwrap_or(true);
+
+    if no_fields {
+        return Err(ApiError::BadRequest(
+            "Minimal satu field valid harus diisi".into(),
+        ));
+    }
+
+    let user = update_user_service(&user_id, data, &db).await?;
+    let user_response: UserResponse = user.into();
+
     Ok(HttpResponse::Ok().json(serde_json::json!({
         "status": "success",
-        "data": user_response
+        "data": user_response,
+        "code": 200
     })))
 }
