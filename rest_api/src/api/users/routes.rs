9c6b89178e61dc23e277e1942f81bd298fbf1afc
ucuppppp <radithamzah12@gmail.com>
2025-06-18 08:23:44 +0700

    feat(backend/rest/user): adding new routes to patch user

--- rest_api/src/api/users/routes.rs
+++ rest_api/src/api/users/routes.rs
@@ -1,12 +1,13 @@
 use actix_web::web;
 
-use super::handler::{get_user_handler, get_users_handler, post_user_handler};
+use super::handler::{get_user_handler, get_users_handler, patch_user_handler, post_user_handler};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/users")
             .route("", web::get().to(get_users_handler))
             .route("", web::post().to(post_user_handler))
-            .route("{id}", web::get().to(get_user_handler)),
+            .route("{id}", web::get().to(get_user_handler))
+            .route("{id}", web::patch().to(patch_user_handler)),
     );
 }
