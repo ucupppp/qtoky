5185e56c1b021f02b1c9723a25df029d4904f7e9
ucuppppp <radithamzah12@gmail.com>
2025-06-18 08:27:05 +0700

    feat(backend/service): adding new service to edit or updating a user

--- rest_api/src/services/user_service.rs
+++ rest_api/src/services/user_service.rs
@@ -1,5 +1,5 @@
 use crate::errors::ServiceError;
-use crate::models::user::{CreateUserDTO, User};
+use crate::models::user::{CreateUserDTO, UpdateUserDTO, User};
 use crate::utils::{handle_duplicate_key_error, hash_password, string_id_to_obj_id};
 use futures::stream::TryStreamExt;
 use mongodb::{Collection, Database, bson::doc};
@@ -25,11 +25,11 @@ pub async fn get_users_service(db: &Database) -> Result<Vec<User>, ServiceError>
     Ok(users)
 }
 
-pub async fn get_user_service(user_id: &str, db: &Database) -> Result<User, ServiceError> {
+pub async fn get_user_service(id: &str, db: &Database) -> Result<User, ServiceError> {
     let collection: Collection<User> = db.collection("users");
 
-    let object_id = match string_id_to_obj_id(user_id) {
-        Some(id) => id,
+    let object_id = match string_id_to_obj_id(id) {
+        Some(user_id) => user_id,
         None => return Err(ServiceError::InvalidId("Invalid ID".into())),
     };
 
@@ -40,9 +40,7 @@ pub async fn get_user_service(user_id: &str, db: &Database) -> Result<User, Serv
         .await
         .map_err(|e| ServiceError::DatabaseError(e.to_string()))?;
 
-    user.ok_or_else(|| {
-        ServiceError::NotFound(format!("User dengan ID '{}' tidak ditemukan", user_id))
-    })
+    user.ok_or_else(|| ServiceError::NotFound(format!("User dengan ID '{}' tidak ditemukan", id)))
 }
 
 pub async fn create_user_service(
@@ -83,4 +81,59 @@ pub async fn create_user_service(
             Err(ServiceError::DatabaseError(err.to_string()))
         }
     }
+}
+
+pub async fn update_user_service(
+    id: &str,
+    payload: UpdateUserDTO,
+    db: &Database,
+) -> Result<User, ServiceError> {
+    let object_id = match string_id_to_obj_id(id) {
+        Some(oid) => oid,
+        None => return Err(ServiceError::InvalidId("Invalid ID".into())),
+    };
+
+    let mut update_doc = doc! {};
+
+    if let Some(username) = payload.username {
+        update_doc.insert("username", username);
+    }
+    if let Some(email) = payload.email {
+        update_doc.insert("email", email);
+    }
+    if let Some(phone_number) = payload.phone_number {
+        update_doc.insert("phone_number", phone_number);
+    }
+
+    if let Some(password) = payload.password {
+        let hashed = hash_password(&password)
+            .map_err(|e| ServiceError::HashingError(format!("Gagal hashing password: {}", e)))?;
+        update_doc.insert("password_hash", hashed);
+    }
+
+    if update_doc.is_empty() {
+        return Err(ServiceError::BadRequest(
+            "Tidak ada data untuk di-update".to_string(),
+        ));
+    }
+
+    let collection: Collection<User> = db.collection("users");
+
+    collection
+        .update_one(doc! { "_id": object_id }, doc! { "$set": update_doc })
+        .await
+        .map_err(|err| {
+            if let Some(conflict_error) = handle_duplicate_key_error(&err) {
+                return conflict_error;
+            }
+            ServiceError::DatabaseError(err.to_string())
+        })?;
+
+    let updated_user = collection
+        .find_one(doc! { "_id": object_id })
+        .await
+        .map_err(|e| ServiceError::DatabaseError(e.to_string()))?
+        .ok_or_else(|| ServiceError::NotFound("User tidak ditemukan".to_string()))?;
+
+    Ok(updated_user)
 }
