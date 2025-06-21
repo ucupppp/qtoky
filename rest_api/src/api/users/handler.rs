0b9194934369d2d4a499f402113f48a5d0286994
ucuppppp <radithamzah12@gmail.com>
2025-06-21 19:16:35 +0700

    feat(backend/rest/user): adding new handler to deleting user

--- rest_api/src/api/users/handler.rs
+++ rest_api/src/api/users/handler.rs
@@ -1,13 +1,15 @@
 use crate::errors::ApiError;
 use crate::models::user::{CreateUserDTO, UpdateUserDTO, UserResponse};
 use crate::services::user_service::{
-    create_user_service, get_user_service, get_users_service, update_user_service,
+    create_user_service, delete_user_service, get_user_service, get_users_service,
+    update_user_service,
 };
 use actix_web::{
     HttpResponse, Result,
     web::{Data, Json, Path},
 };
 use mongodb::Database;
+use validator::Validate;
 
 pub async fn get_user_handler(
     path: Path<String>,
@@ -37,10 +39,15 @@ pub async fn get_users_handler(db: Data<Database>) -> Result<HttpResponse, ApiEr
 }
 
 pub async fn post_user_handler(
-    payload: Json<CreateUserDTO>,
+    payload: Option<Json<CreateUserDTO>>,
     db: Data<Database>,
 ) -> Result<HttpResponse, ApiError> {
-    let new_user = create_user_service(payload.into_inner(), &db).await?;
+    let Some(json_payload) = payload else {
+        return Err(ApiError::BadRequest("Body tidak boleh kosong".into()));
+    };
+    let data = json_payload.into_inner();
+    data.validate()?;
+    let new_user = create_user_service(data, &db).await?;
     let user_response: UserResponse = new_user.into();
     Ok(HttpResponse::Created()
         .append_header(("Location", format!("/users/{}", user_response.id)))
@@ -63,7 +70,7 @@ pub async fn patch_user_handler(
     };
 
     let data = json_payload.into_inner();
-
+    data.validate()?;
     // Validasi semua field kosong atau berisi string kosong
     let no_fields = data
         .username
@@ -99,5 +106,17 @@ pub async fn patch_user_handler(
         "status": "success",
         "data": user_response,
         "code": 200
+    })))
+}
+
+pub async fn delete_user_handler(
+    path: Path<String>,
+    db: Data<Database>,
+) -> Result<HttpResponse, ApiError> {
+    let user_id = path.into_inner();
+    let delete_user = delete_user_service(&user_id, &db).await?;
+    Ok(HttpResponse::Ok().json(serde_json::json!({
+        "status": "success",
+        "code": 204
     })))
 }
