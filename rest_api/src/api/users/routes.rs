60092829ff267a245fd2c54abe194ffffc4de0bc
ucuppppp <radithamzah12@gmail.com>
2025-06-22 03:34:30 +0700

    feat(backend/rest/user): implementing auth middleware on user routes

--- rest_api/src/api/users/routes.rs
+++ rest_api/src/api/users/routes.rs
@@ -1,12 +1,13 @@
-use actix_web::web;
-
 use super::handler::{
     delete_user_handler, get_user_handler, get_users_handler, patch_user_handler, post_user_handler,
 };
+use crate::middlewares::auth_middleware::AuthMiddleware;
+use actix_web::web;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/users")
+            .wrap(AuthMiddleware)
             .route("", web::get().to(get_users_handler))
             .route("", web::post().to(post_user_handler))
             .route("{id}", web::get().to(get_user_handler))
