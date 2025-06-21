f2c5d3b4d066cc93f2c998838b6867a52232ba72
ucuppppp <radithamzah12@gmail.com>
2025-06-22 00:35:37 +0700

    refactor(backend): changing api routes for better structure

--- rest_api/src/api/mod.rs
+++ rest_api/src/api/mod.rs
@@ -1,1 +1,11 @@
-pub mod users;
+use actix_web::web;
+mod auth;
+mod users;
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api")
+            .configure(users::routes::config)
+            .configure(auth::routes::config),
+    );
+}
