3fc61182389f8a3769bc7268eb96659e4896aad9
ucuppppp <radithamzah12@gmail.com>
2025-06-28 16:41:57 +0700

    feat(backend/rest/auth): adding new route for register user

--- rest_api/src/api/auth/routes.rs
+++ rest_api/src/api/auth/routes.rs
@@ -1,7 +1,11 @@
 use actix_web::web;
 
-use super::handler::login_handler;
+use super::handler::{login_handler, register_handler};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/auth").route("/login", web::post().to(login_handler)));
+    cfg.service(
+        web::scope("/auth")
+            .route("/login", web::post().to(login_handler))
+            .route("/register", web::post().to(register_handler)),
+    );
 }
