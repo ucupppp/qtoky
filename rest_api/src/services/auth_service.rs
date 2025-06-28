4333bbb365e480b45e409a177e6202f7e8a2cb3d
ucuppppp <radithamzah12@gmail.com>
2025-06-28 16:36:57 +0700

    feat(backend/service): adding new service to register user

--- rest_api/src/services/auth_service.rs
+++ rest_api/src/services/auth_service.rs
@@ -1,6 +1,8 @@
 use crate::errors::ServiceError;
-use crate::models::user::{LoginDTO, User};
-use crate::utils::verify_password;
+use crate::models::user::{LoginDTO, RegisterDTO, User};
+use crate::utils::{
+    handle_duplicate_key_error, hash_password, string_id_to_obj_id, verify_password,
+};
 use mongodb::{Collection, Database, bson::doc};
 
 pub async fn login_service(payload: LoginDTO, db: &Database) -> Result<User, ServiceError> {
@@ -29,3 +31,36 @@ pub async fn login_service(payload: LoginDTO, db: &Database) -> Result<User, Ser
     Ok(user)
 }
 
+pub async fn register_service(payload: RegisterDTO, db: &Database) -> Result<User, ServiceError> {
+    let collection: Collection<User> = db.collection("users");
+
+    let RegisterDTO {
+        username,
+        email,
+        phone_number,
+        password,
+    } = payload;
+
+    let hashed_password = hash_password(&password)
+        .map_err(|e| ServiceError::HashingError(format!("Gagal hashing password: {}", e)))?;
+
+    let new_user = User {
+        id: None,
+        username,
+        email,
+        password_hash: hashed_password,
+        phone_number,
+    };
+
+    let result = collection.insert_one(&new_user).await;
+    match result {
+        Ok(_) => Ok(new_user),
+        Err(err) => {
+            if let Some(conflict_error) = handle_duplicate_key_error(&err) {
+                return Err(conflict_error);
+            }
+
+            Err(ServiceError::DatabaseError(err.to_string()))
+        }
+    }
+}
