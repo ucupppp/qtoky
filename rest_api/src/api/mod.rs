0c482381d5f2987b865be153478fe0fd1e40fd76
ucuppppp <radithamzah12@gmail.com>
2025-06-29 17:49:50 +0700

    chore(backend): exporting module api/products, adding dependency

--- rest_api/src/api/mod.rs
+++ rest_api/src/api/mod.rs
@@ -1,11 +1,13 @@
 use actix_web::web;
 mod auth;
+mod products;
 mod users;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api")
             .configure(users::routes::config)
-            .configure(auth::routes::config),
+            .configure(auth::routes::config)
+            .configure(products::routes::config),
     );
 }
