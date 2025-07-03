80b30235050f2490e291e66159ba3712abdb44fe
ucuppppp <radithamzah12@gmail.com>
2025-07-03 23:32:01 +0700

    fix(backend/service): fixing inserting data updated_at to db from bson date to string

--- rest_api/src/services/product_service.rs
+++ rest_api/src/services/product_service.rs
@@ -90,13 +90,19 @@ pub async fn create_product_service(
 }
 
 pub async fn update_product_service(
-    id: &str,
+    product_id: &str,
     payload: UpdateProductDTO,
     db: &Database,
+    user_id: &str,
 ) -> Result<Product, ServiceError> {
-    let object_id = match string_id_to_obj_id(id) {
+    let object_id = match string_id_to_obj_id(product_id) {
         Some(oid) => oid,
-        None => return Err(ServiceError::InvalidId("Invalid ID".into())),
+        None => return Err(ServiceError::InvalidId("Invalid product ID".into())),
+    };
+
+    let user_object_id = match string_id_to_obj_id(user_id) {
+        Some(oid) => oid,
+        None => return Err(ServiceError::InvalidId("Invalid user ID".into())),
     };
 
     let mut update_doc = doc! {};
@@ -124,13 +130,20 @@ pub async fn update_product_service(
     }
 
     let now = Utc::now();
+    update_doc.insert("updated_at", now.to_rfc3339());
 
-    update_doc.insert("updated_at", BsonDateTime::from_chrono(now));
+    println!("{:?}", &update_doc);
 
     let collection: Collection<Product> = db.collection("products");
 
-    collection
-        .update_one(doc! { "_id": object_id }, doc! { "$set": update_doc })
+    // Tambahkan filter user_id di sini
+    let filter = doc! {
+        "_id": object_id,
+        "user_id": user_object_id,
+    };
+
+    let update_result = collection
+        .update_one(filter.clone(), doc! { "$set": update_doc })
         .await
         .map_err(|err| {
             if let Some(conflict_error) = handle_duplicate_key_error(&err) {
@@ -139,11 +152,24 @@ pub async fn update_product_service(
             ServiceError::DatabaseError(err.to_string())
         })?;
 
+    println!("{:?}", &update_result);
+
+    if update_result.matched_count == 0 {
+        return Err(ServiceError::NotFound(
+            "Produk tidak ditemukan atau tidak dimiliki oleh user ini".to_string(),
+        ));
+    }
+
     let updated_product = collection
-        .find_one(doc! { "_id": object_id })
+        .find_one(filter)
         .await
-        .map_err(|e| ServiceError::DatabaseError(e.to_string()))?
-        .ok_or_else(|| ServiceError::NotFound("Product tidak ditemukan!".to_string()))?;
+        .map_err(|e| {
+            println!("{:?}", e);
+            ServiceError::DatabaseError(e.to_string())
+        })?
+        .ok_or_else(|| ServiceError::NotFound("Produk tidak ditemukan!".to_string()))?;
+
+    println!("{:?}", &updated_product);
 
     Ok(updated_product)
 }
