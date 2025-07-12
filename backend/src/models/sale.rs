c5bbc91199d0eb01b1c59836b0d03b6fac247b71
ucuppppp <radithamzah12@gmail.com>
2025-07-13 00:37:15 +0700

    feat(backend/models/sale): adding new several structs model

--- backend/src/models/sale.rs
+++ backend/src/models/sale.rs
@@ -41,7 +41,7 @@ pub struct Sale {
 
     #[serde(serialize_with = "opt_object_id_as_string")]
     pub payment_method_id: Option<ObjectId>,
-    pub sale_date: DateTime,
+    pub sale_date: Option<DateTime>,
     pub notes: Option<String>,
 
     #[serde(default)]
@@ -58,7 +58,7 @@ pub struct SaleItemDTO {
     pub quantity: i32,
 
     #[validate(range(min = 100.0, message = "Harga minimal 100"))]
-    pub price: f64,
+    pub price: Option<f64>,
 
     #[validate(range(min = 0.0, message = "Diskon tidak boleh negatif"))]
     pub discount: Option<f64>,
@@ -80,3 +80,62 @@ pub struct SaleDTO {
     #[validate(length(min = 0, max = 255, message = "Catatan maksimal 255 karakter"))]
     pub notes: Option<String>,
 }
+
+
+
+
+#[derive(Debug, Serialize)]
+pub struct SaleResponse {
+    pub id: String,
+    pub user_id: String,
+
+    pub customer_id: Option<String>,
+    pub items: Vec<SaleItem>,
+
+    pub total_amount: f64,
+    pub discount_total: f64,
+    pub final_amount: f64,
+
+    pub paid_amount: f64,
+    pub remaining_amount: f64,
+    pub status: String,
+
+    pub invoice_number: Option<String>,
+    pub payment_method_id: Option<String>,
+    pub sale_date: Option<String>,
+    pub notes: Option<String>,
+
+    #[serde(default)]
+    pub created_at: Option<String>,
+    #[serde(default)]
+    pub updated_at: Option<String>,
+}
+
+
+impl From<Sale> for SaleResponse {
+    fn from(sale: Sale) -> Self {
+        SaleResponse {
+            id: sale.id.expect("Sale.id harus ada").to_hex(),
+            user_id: sale.user_id.to_hex(),
+            customer_id: sale.customer_id.map(|id| id.to_hex()),
+            items: sale.items,
+
+            total_amount: sale.total_amount,
+            discount_total: sale.discount_total,
+            final_amount: sale.final_amount,
+
+            paid_amount: sale.paid_amount,
+            remaining_amount: sale.remaining_amount,
+            status: sale.status,
+
+            invoice_number: sale.invoice_number,
+            payment_method_id: sale.payment_method_id.map(|id| id.to_hex()),
+            sale_date: sale.sale_date.map(|t| t.to_chrono().to_rfc3339()),
+            notes: sale.notes,
+
+            created_at: sale.created_at.map(|t| t.to_chrono().to_rfc3339()),
+            updated_at: sale.updated_at.map(|t| t.to_chrono().to_rfc3339()),
+        }
+    }
+}
+
