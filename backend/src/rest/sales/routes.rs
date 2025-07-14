64b6b74ab4694f46bde990dfcc919dfc3562dfe9
ucupppp <kanetodayki124@gmail.com>
2025-07-15 03:41:07 +0700

    feat(backend/rest/sales): new handler and route for get sales

--- backend/src/rest/sales/routes.rs
+++ backend/src/rest/sales/routes.rs
@@ -1,5 +1,4 @@
-
-use super::handler::{post_sale_handler};
+use super::handler::{get_sales_handler, post_sale_handler};
 use crate::middlewares::auth_middleware::AuthMiddleware;
 use actix_web::web;
 
@@ -7,10 +6,9 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/sales")
             .wrap(AuthMiddleware)
-            // .route("", web::get().to(get_products_handler))
-            .route("", web::post().to(post_sale_handler))
-            // .route("{id}", web::get().to(get_product_handler))
-            // .route("{id}", web::patch().to(patch_product_handler))
-            // .route("{id}", web::delete().to(delete_product_handler)),
+            .route("", web::get().to(get_sales_handler))
+            .route("", web::post().to(post_sale_handler)), // .route("{id}", web::get().to(get_product_handler))
+                                                           // .route("{id}", web::patch().to(patch_product_handler))
+                                                           // .route("{id}", web::delete().to(delete_product_handler)),
     );
 }
